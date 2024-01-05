a45601983101a6e5c1d1f511bc6a84d4c4f40bd6
William Pei Yuan <williampeiyuan@gmail.com>
2024-01-05 14:02:11 -0500

    test

--- src.js
+++ src.js
@@ -1,22 +1,10 @@
 const dampen = (wait, action) => {
-  let args = []
-  let disp = Function.prototype
-  let timeout = 0
-
-  const dampened = (...nextArgs) => dispatch => {
-    args = nextArgs
-    disp = dispatch
-
-    clearTimeout(timeout)
-    timeout = setTimeout(run, wait)
-  }
-
   const run = () => {
-    timeout = 0
-    disp(action(...args))
-  }
+    timeout = 0;
+    disp(action(...args));
+  };
 
-  return dampened
-}
+  return dampened;
+};
 
-module.exports = dampen
+module.exports = dampen;

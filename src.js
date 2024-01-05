fa87c96c735e7783ebd3b29e3b98a00279c51cca
William Pei Yuan <williampeiyuan@gmail.com>
2024-01-05 14:19:43 -0500

    rando

--- src.js
+++ src.js
@@ -1,4 +1,18 @@
 const dampen = (wait, action) => {
+  let args = [];
+  let disp = Function.prototype;
+  let timeout = 0;
+
+  const dampened =
+    (...nextArgs) =>
+    (dispatch) => {
+      args = nextArgs;
+      disp = dispatch;
+
+      clearTimeout(timeout);
+      timeout = setTimeout(run, wait);
+    };
+
   const run = () => {
     timeout = 0;
     disp(action(...args));
